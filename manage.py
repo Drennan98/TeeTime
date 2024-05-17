6f597c72e7945648bb9617270179a49528775739
Darragh Drennan <darraghdrennan5@gmail.com>
2024-05-17 12:13:47 +0000

    Make migrations

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'teetime.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tee_time.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
