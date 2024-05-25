009121ae3c8422218acddffc97ca8811c9f5a189
Darragh Drennan <darraghdrennan5@gmail.com>
2024-05-25 13:30:51 +0000

    Add hero unit on landing page

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,14 @@
 from django.views import generic
-from .models import Post
+from .models import Post, Comment
+from django.core.paginator import Paginator
 
 # Create your views here.
 
 # Class based views for mapping 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by("-created_on")
-    template_name = "index.html"
+    template_name = "home.html"
+    paginate_by = 5
 
 class PostDetail(generic.DetailView):
     model = Post
