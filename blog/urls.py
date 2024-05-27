74db004906f97698d8f676c4bb5accab70adf5e8
Darragh Drennan <darraghdrennan5@gmail.com>
2024-05-27 12:35:50 +0000

    Add register.html and sign up form

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('post/<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('register/', views.signup, name='register')
 ]
 
 if settings.DEBUG:
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 from django.views import generic
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post, Comment
 from django.core.paginator import Paginator
+from contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 # Create your views here.
 
@@ -20,3 +23,31 @@ def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug)
     queryset = Post.objects.filter(status=1)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def signup(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        confirm_password = request.POST["confirm_password"]
+
+        if password != confirm_password:
+            messages.error (request, "The passwords do not match. Please try again.")
+            return render (request, "form.html")
+        
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "Username already in use. Please try again.")
+            return render (request, "form.html")
+        
+        if User.objects.filter(email=email).exists():
+            messages.error(request, "Email already in use. Please try again.")
+            return render (request, "form.html")
+        
+        user = User.objects.create_user(username=username, email=email, password=password)
+        user.save()
+
+        # The user is sent back to home page upon successful signup.
+        login(request, user)
+        return redirect("home")
+    
+    return render (request, "home.html")
