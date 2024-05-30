435a1983a141dbd95c49b99df7b2e31588b1c48c
Darragh Drennan <darraghdrennan5@gmail.com>
2024-05-30 18:00:06 +0000

    Configure settings for static files

--- blog/admin.py
+++ blog/admin.py
@@ -6,9 +6,9 @@ from .models import Post, Category, Comment
 # Basic customization
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ("title", "slug", "status", "created_on")
+    list_display = ("title", "slug", "status", "created_on", "category")
     list_filter = ("status",)
-    search_fields = ["title", "content"]
+    search_fields = ["title", "content", "category"]
     prepopulated_fields = {'slug': ('title',)}
 
 admin.site.register(Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,28 +1,26 @@
-from time import timezone
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
-from cloudinary.models import CloudinaryField
 
-# Create your models here.
-
-# Used for filtering posts based on their status
 STATUS = (
     (0, "Draft"),
     (1, "Publish")
 )
 
-# Post model with basic structure.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    # Moved line starting with "user" down to be pep8 compliant
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='golf_posts')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='golf_posts')
     updated_on = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    featured_image = CloudinaryField('#', default='placeholder text...')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='posts', null=True, blank=True)
 
     def publish(self):
         self.published_date = timezone.now()
@@ -30,8 +28,10 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-    
-# Comment model with basic structure
+
+    class Meta:
+        ordering = ['-created_on']
+
 class Comment(models.Model):
     post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
     name = models.CharField(max_length=80)
@@ -41,22 +41,16 @@ class Comment(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     active = models.BooleanField(default=True)
 
-    class Meta: 
+    class Meta:
         ordering = ['created_on']
 
     def __str__(self):
         return f"Comment by {self.name} on {self.post}"
-    
-# Golf category with basic structure
-class Category(models.Model):
-    name = models.CharField(max_length=255)
 
-    def __str__(self):
-        return self.name
 
-# The '-' before "created_on" is used so the most recently published posts will show up first.
-class Meta:
-    ordering = ['-created_on']
+
+
+
 
 
 
