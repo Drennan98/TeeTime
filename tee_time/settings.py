f6bd78c8e9c388b66d5840bf11a00f807e19a295
Darragh Drennan <darraghdrennan5@gmail.com>
2024-06-02 18:21:02 +0000

    Create app and signup apps

--- tee_time/settings.py
+++ tee_time/settings.py
@@ -139,8 +139,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'statica'),]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -151,5 +151,5 @@ MEDIA_URL ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 ACCOUNT_FORMS = {
-    'signup': 'myapp.forms.CustomSignupForm',
+    'signup': 'tee_time.forms.CustomSignupForm',
 }
