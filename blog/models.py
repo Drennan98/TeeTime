7f0204cecdccb47381ab3a3262811cf8ce08faa5
Darragh Drennan <darraghdrennan5@gmail.com>
2024-06-02 17:14:42 +0000

    Remove updated on field for Post class

--- blog/models.py
+++ blog/models.py
@@ -17,7 +17,6 @@ class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='golf_posts')
-    updated_on = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='posts', null=True, blank=True)
