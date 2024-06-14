338ea8ca93bd6a306f71030b4a6e9493bb6a1d73
Darragh Drennan <darraghdrennan5@gmail.com>
2024-06-14 18:21:24 +0000

    Delete golfdata app

--- golfdata/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class GolfdataConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'golfdata'
--- golfdata/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('live-scores/', views.live_scores, name='live_scores'),
-]
