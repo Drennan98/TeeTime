d63bf7d69c8c7b1439361403db73e5813d13aecc
Darragh Drennan <darraghdrennan5@gmail.com>
2024-06-14 17:45:52 +0000

    Create necessary views to handle golf course form

--- home/views.py
+++ home/views.py
@@ -2,3 +2,26 @@ from django.shortcuts import render
 
 # Create your views here.
 
+class GolfCourseListView(ListView):
+    model = GolfCourse
+    template_name = "golf_course.html"
+
+class GolfCourseDetailView(DetailView):
+    model = GolfCourse 
+    template_name = "golf_course.html"
+
+class GolfCourseCreateView(CreateView):
+    model = GolfCourse
+    form_class = GolfCourseForm
+    template_name = "golf_course.html"
+    success_url = reverse_lazy("golfcourse_list")
+
+class GolfCourseUpdateView(UpdateView):
+    model = GolfCourse 
+    form_class = GolfCourseForm
+    template_name = "golf_course.html"
+    success_url = reverse_lazy("golfcourse_list")
+
+class GolfCourseDeleteView(DeleteView):
+    model = GolfCourse 
+    template_name = "golf_course.html"
