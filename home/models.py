4f2844f3f375b82bd7c6ab5bc728d21357216bc5
Darragh Drennan <darraghdrennan5@gmail.com>
2024-06-14 17:38:00 +0000

    Add golf course model to home app

--- home/models.py
+++ home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+# Golf course model for users
+class GolfCourse(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    par = models.IntegerField()
+    holes = models.IntegerField()
+
+    def __str__(self):
+        return self.name
