8700149fa588f4827c5d21d903d5a618dd0f124a
Darragh Drennan <darraghdrennan5@gmail.com>
2024-06-17 18:12:08 +0000

    Fix Golf Course link

--- home/urls.py
+++ home/urls.py
@@ -13,10 +13,10 @@ from django.conf.urls.static import static
 from blog.views import PostDetail, PostList
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
-    path('home/', views.PostList.as_view(), name='home'),
+    path('', PostList.as_view(), name='home'),
+    path('home/', PostList.as_view(), name='home'),
     path('list/', GolfCourseListView.as_view(), name='golfcourse_list'),
-    path('<int:pk/', GolfCourseDetailView.as_view(), name='golfcourse_detail'),
+    path('<int:pk>/', GolfCourseDetailView.as_view(), name='golfcourse_detail'),
     path('create/', GolfCourseCreateView.as_view(), name='golfcourse_create'),
     path('<int:pk>/update/', GolfCourseUpdateView.as_view(), name='golfcourse_update'),
     path('<int:pk>/delete/', GolfCourseDeleteView.as_view(), name='golfcourse_delete'),
--- tee_time/urls.py
+++ tee_time/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('accounts/', include('allauth.urls')),
+    path('', include('home.urls')),
 ]
