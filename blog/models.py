0899ce4d5e8a97286e62d3b99859a4464aa38b0a
Darragh Drennan <darraghdrennan5@gmail.com>
2024-07-01 15:15:30 +0000

    Import autoslug for unique slug generation

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from autoslug import AutoSlugField
 
 STATUS = (
     (0, "Draft"),
@@ -10,7 +11,7 @@ STATUS = (
 # This is my blog post model. Users can share posts. 
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=200, unique=True)
+    slug = AutoSlugField(populate_from='title', unique=True, always_update=True)
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='golf_posts')
     created_on = models.DateTimeField(auto_now_add=True)
