0b328fb2830c4147630a000fc223109090aae6ce
Darragh Drennan <darraghdrennan5@gmail.com>
2024-07-02 18:13:23 +0000

    Remove preppopulated fields in admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -9,7 +9,6 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ("title", "slug", "status", "created_on")
     list_filter = ("status",)
     search_fields = ["title", "content", "category"]
-    prepopulated_fields = {'slug': ('title',)}
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(Course)
